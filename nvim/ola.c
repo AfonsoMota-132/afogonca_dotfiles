134c6038f264c757851a41f6d2bd155e58dab64b
afogonca <afonsomota88@gmail.com>
2025-10-25 15:48:30 +0100

    res

--- nvim/ola.c
+++ nvim/ola.c
@@ -1,7 +1,8 @@
 
-int	main(int ac, char **av) {
-  if (ac == 2) {
-    printf("%s\n", av[1]);
-  }
-  return (0);
+int	main(int ac, char **av)
+{
+	if (ac == 2){
+		printf("%s\n", av[1]);
+	}
+	return (0);
 }
